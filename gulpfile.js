f669fac4fea413330da47f3a75e47baac682acde
Andrew Jarrett <aj@Andrews-Air.attlocal.net>
2016-06-15 18:23:57 -0500

    add autoprefixer for clean-styles task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,60 @@
+// TODO: import gulp.config.js file
+
+'use strict';
+
 var gulp       = require('gulp'),
-    browserify = require('browserify'),
-    del        = require('del'),
-    concat     = require('gulp-concat'),
-    gutil      = require('gulp-util'),
-    jshint     = require('gulp-jshint'),
-    livereload = require('gulp-livereload'),
-    nodemon    = require('gulp-nodemon'),
-    plumber    = require('gulp-plumber'),
-    sass       = require('gulp-ruby-sass'),
-    source     = require('vinyl-source-stream');
+		autoprefix = require('gulp-autoprefixer'),
+		browserify = require('browserify'),
+		// config     = require('./gulp.config'),
+		del        = require('del'),
+		concat     = require('gulp-concat'),
+		gutil      = require('gulp-util'),
+		jshint     = require('gulp-jshint'),
+		livereload = require('gulp-livereload'),
+		nodemon    = require('gulp-nodemon'),
+		plumber    = require('gulp-plumber'),
+		sass       = require('gulp-ruby-sass'),
+		source     = require('vinyl-source-stream');
 
 var config = {
-  css: './public/css/',
-  js: './public/js/',
-  lib: './public/lib/',
-  allJs: [
-    './public/js/src/**/*.js',
-    './*.js',
-    './app/**/*.js'
-  ]
+	allJs: [
+		'./public/js/src/**/*.js',
+		'./*.js',
+		'./app/**/*.js'
+	],
+	css: './public/css/',
+	js: './public/js/',
+	lib: './public/lib/',
+	sass: './public/css/**/*.scss'
 };
 
 gulp.task('default', [
-  'styles',
-  'scripts',
-  'develop',
-  'watch'
+	'styles',
+	'scripts',
+	'develop',
+	'watch'
 ]);
 
+gulp.task('clean-styles', function(){
+	var files = config.css + '*.css';
+	del(files);
+});
+
 gulp.task('develop', function () {
-  livereload.listen();
-  nodemon({
-    script: 'app.js',
-    ext: 'js jade',
-    stdout: false
-  }).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
-        livereload.changed(__dirname);
-      }
-    });
-    this.stdout.pipe(process.stdout);
-    this.stderr.pipe(process.stderr);
-  });
+	livereload.listen();
+	nodemon({
+		script: 'app.js',
+		ext: 'js jade',
+		stdout: false
+	}).on('readable', function () {
+		this.stdout.on('data', function (chunk) {
+			if(/^Express server listening on port/.test(chunk)){
+				livereload.changed(__dirname);
+			}
+		});
+		this.stdout.pipe(process.stdout);
+		this.stderr.pipe(process.stderr);
+	});
 });
 
 // gulp.task('bundle', function(){
@@ -56,28 +68,30 @@ gulp.task('develop', function () {
 // });
 
 gulp.task('scripts', ['vet'], function(){
-  return gulp.src(config.js + 'main.js')
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest(config.js));
+
+	return gulp.src(config.js + 'main.js')
+		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest(config.js));
 });
 
 gulp.task('styles', function () {
-  return sass(config.css + '**/*.scss')
-    .pipe(gulp.dest(config.css))
-    .pipe(livereload());
+	return sass(config.css + '**/*.scss')
+		.pipe(autoprefix({browsers: ['last 2 version', '> 5%']}))
+		.pipe(gulp.dest(config.css))
+		.pipe(livereload());
 });
 
 gulp.task('vet', function(){
-  return gulp.src(config.allJs)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish', {
-      verbose: true
-    }));
+	return gulp.src(config.allJs)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish', {
+			verbose: true
+		}));
 });
 
 gulp.task('watch', function() {
-  gulp.watch(config.css + '**/*.scss', ['styles']);
-  gulp.watch(config.js + '**/*.js', ['scripts']);
+	gulp.watch(config.css + '**/*.scss', ['styles']);
+	gulp.watch(config.js + '**/*.js', ['scripts']);
 });
 
 
